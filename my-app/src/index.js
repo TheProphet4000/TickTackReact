dcad54a2913bf311c9b5b83599f933f5de6aa274
Mikkel Andreasen <38246850+TheProphet4000@users.noreply.github.com>
2021-12-06 11:52:09 +0100

    ?

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,3 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
+import React from 'react';
+// eslint-disable-next-line
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class Square extends React.Component{
+    render(){
+        return(
+        <button className="square" onClick={function(){
+            console.log("clicked"); }}>
+            {this.props.value}
+            </button>
+        );
+    }
+}
+// eslint-disable-next-line
+class Board extends React.Component{
+    renderSquare(i){
+        return <Square value={i} />;
+    }
+}
